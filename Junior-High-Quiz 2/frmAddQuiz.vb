4fe01cc9bf7dc5a0f2b1a27832e4e31a8dbe4c72
MarkDino21 <markdinopelonia447@gmail.com>
2018-10-02 15:22:53 +0800

    fix the layout & colors, remove unuseable codes

--- Junior-High-Quiz 2/frmAddQuiz.vb
+++ Junior-High-Quiz 2/frmAddQuiz.vb
@@ -1,26 +1,7 @@
 ﻿Public Class frmAddQuiz
     Private Sub frmAddQuiz_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        ''TODO: This line of code loads data into the 'QuizDataSet.Values' table. You can move, or remove it, as needed.
-        'Me.ValuesTableAdapter.Fill(Me.QuizDataSet.Values)
-        ''TODO: This line of code loads data into the 'QuizDataSet.TLE' table. You can move, or remove it, as needed.
-        'Me.TLETableAdapter.Fill(Me.QuizDataSet.TLE)
-        ''TODO: This line of code loads data into the 'QuizDataSet.Science' table. You can move, or remove it, as needed.
-        'Me.ScienceTableAdapter.Fill(Me.QuizDataSet.Science)
-        ''TODO: This line of code loads data into the 'QuizDataSet.Math' table. You can move, or remove it, as needed.
-        'Me.MathTableAdapter.Fill(Me.QuizDataSet.Math)
-        ''TODO: This line of code loads data into the 'QuizDataSet.Mapeh' table. You can move, or remove it, as needed.
-        'Me.MapehTableAdapter.Fill(Me.QuizDataSet.Mapeh)
-        ''TODO: This line of code loads data into the 'QuizDataSet.Filipino' table. You can move, or remove it, as needed.
-        'Me.FilipinoTableAdapter.Fill(Me.QuizDataSet.Filipino)
-        ''TODO: This line of code loads data into the 'QuizDataSet.English' table. You can move, or remove it, as needed.
-        'Me.EnglishTableAdapter.Fill(Me.QuizDataSet.English)
-        ''TODO: This line of code loads data into the 'QuizDataSet.Copy_Of_Math' table. You can move, or remove it, as needed.
-        'Me.Copy_Of_MathTableAdapter.Fill(Me.QuizDataSet.Copy_Of_Math)
-        ''TODO: This line of code loads data into the 'QuizDataSet.Computer' table. You can move, or remove it, as needed.
-        'Me.ComputerTableAdapter.Fill(Me.QuizDataSet.Computer)
-        ''TODO: This line of code loads data into the 'QuizDataSet.AP' table. You can move, or remove it, as needed.
-        'Me.APTableAdapter.Fill(Me.QuizDataSet.AP)
-
+        disableEdit()
+        btnAdd.Enabled = False
     End Sub
 
     Private Sub cboSubj_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cboSubj.SelectedIndexChanged
@@ -46,6 +27,7 @@
         Me.APTableAdapter.Fill(Me.QuizDataSet.AP)
 
         BSquiz.DataMember = cboSubj.Text
+        btnAdd.Enabled = True
     End Sub
 
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
@@ -124,8 +106,8 @@
         txtNotes.Enabled = True
     End Sub
 
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        frmQuiz.Show()
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Close()
     End Sub
 
     Private Sub txtID_TextChanged(sender As Object, e As EventArgs) Handles txtID.TextChanged
