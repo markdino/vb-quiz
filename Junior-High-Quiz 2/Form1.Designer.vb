e67e5d2f43412bee8829cc0aac40a6083e4f712a
MarkDino21 <markdinopelonia447@gmail.com>
2018-10-06 11:36:06 +0800

    set mainForm Responsive layout and Design with datagrid hidden

--- Junior-High-Quiz 2/Form1.Designer.vb
+++ Junior-High-Quiz 2/Form1.Designer.vb
@@ -22,29 +22,346 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.Button1 = New System.Windows.Forms.Button()
+        Me.components = New System.ComponentModel.Container()
+        Me.btnAP = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.IDDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.QuizDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.ADataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.BDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.CDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.NotesDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.TakeDataGridViewCheckBoxColumn = New System.Windows.Forms.DataGridViewCheckBoxColumn()
+        Me.CorrectDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.QuizDataSetBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+        Me.QuizDataSet = New Junior_High_Quiz_2.quizDataSet()
+        Me.APTableAdapter = New Junior_High_Quiz_2.quizDataSetTableAdapters.APTableAdapter()
+        Me.ComputerTableAdapter = New Junior_High_Quiz_2.quizDataSetTableAdapters.ComputerTableAdapter()
+        Me.EnglishTableAdapter = New Junior_High_Quiz_2.quizDataSetTableAdapters.EnglishTableAdapter()
+        Me.FilipinoTableAdapter = New Junior_High_Quiz_2.quizDataSetTableAdapters.FilipinoTableAdapter()
+        Me.MapehTableAdapter = New Junior_High_Quiz_2.quizDataSetTableAdapters.MapehTableAdapter()
+        Me.MathTableAdapter = New Junior_High_Quiz_2.quizDataSetTableAdapters.MathTableAdapter()
+        Me.ScienceTableAdapter = New Junior_High_Quiz_2.quizDataSetTableAdapters.ScienceTableAdapter()
+        Me.TLETableAdapter = New Junior_High_Quiz_2.quizDataSetTableAdapters.TLETableAdapter()
+        Me.ValuesTableAdapter = New Junior_High_Quiz_2.quizDataSetTableAdapters.ValuesTableAdapter()
+        Me.btnComp = New System.Windows.Forms.Button()
+        Me.btnEng = New System.Windows.Forms.Button()
+        Me.btnFil = New System.Windows.Forms.Button()
+        Me.btnMath = New System.Windows.Forms.Button()
+        Me.btnSci = New System.Windows.Forms.Button()
+        Me.btnTLE = New System.Windows.Forms.Button()
+        Me.btnMap = New System.Windows.Forms.Button()
+        Me.btnVal = New System.Windows.Forms.Button()
+        Me.mainPanel = New System.Windows.Forms.Panel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.QuizDataSetBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.QuizDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.mainPanel.SuspendLayout()
         Me.SuspendLayout()
         '
-        'Button1
+        'btnAP
         '
-        Me.Button1.Location = New System.Drawing.Point(370, 136)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(201, 70)
-        Me.Button1.TabIndex = 0
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.btnAP.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnAP.ForeColor = System.Drawing.Color.White
+        Me.btnAP.Location = New System.Drawing.Point(21, 97)
+        Me.btnAP.Name = "btnAP"
+        Me.btnAP.Size = New System.Drawing.Size(201, 54)
+        Me.btnAP.TabIndex = 0
+        Me.btnAP.Text = "AP"
+        Me.btnAP.UseVisualStyleBackColor = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.AutoGenerateColumns = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IDDataGridViewTextBoxColumn, Me.QuizDataGridViewTextBoxColumn, Me.ADataGridViewTextBoxColumn, Me.BDataGridViewTextBoxColumn, Me.CDataGridViewTextBoxColumn, Me.DDataGridViewTextBoxColumn, Me.NotesDataGridViewTextBoxColumn, Me.TakeDataGridViewCheckBoxColumn, Me.CorrectDataGridViewTextBoxColumn})
+        Me.DataGridView1.DataSource = Me.QuizDataSetBindingSource
+        Me.DataGridView1.Location = New System.Drawing.Point(808, 12)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(401, 143)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'IDDataGridViewTextBoxColumn
+        '
+        Me.IDDataGridViewTextBoxColumn.DataPropertyName = "ID"
+        Me.IDDataGridViewTextBoxColumn.HeaderText = "ID"
+        Me.IDDataGridViewTextBoxColumn.Name = "IDDataGridViewTextBoxColumn"
+        Me.IDDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'QuizDataGridViewTextBoxColumn
+        '
+        Me.QuizDataGridViewTextBoxColumn.DataPropertyName = "Quiz"
+        Me.QuizDataGridViewTextBoxColumn.HeaderText = "Quiz"
+        Me.QuizDataGridViewTextBoxColumn.Name = "QuizDataGridViewTextBoxColumn"
+        Me.QuizDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'ADataGridViewTextBoxColumn
+        '
+        Me.ADataGridViewTextBoxColumn.DataPropertyName = "A"
+        Me.ADataGridViewTextBoxColumn.HeaderText = "A"
+        Me.ADataGridViewTextBoxColumn.Name = "ADataGridViewTextBoxColumn"
+        Me.ADataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'BDataGridViewTextBoxColumn
+        '
+        Me.BDataGridViewTextBoxColumn.DataPropertyName = "B"
+        Me.BDataGridViewTextBoxColumn.HeaderText = "B"
+        Me.BDataGridViewTextBoxColumn.Name = "BDataGridViewTextBoxColumn"
+        Me.BDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'CDataGridViewTextBoxColumn
+        '
+        Me.CDataGridViewTextBoxColumn.DataPropertyName = "C"
+        Me.CDataGridViewTextBoxColumn.HeaderText = "C"
+        Me.CDataGridViewTextBoxColumn.Name = "CDataGridViewTextBoxColumn"
+        Me.CDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'DDataGridViewTextBoxColumn
+        '
+        Me.DDataGridViewTextBoxColumn.DataPropertyName = "D"
+        Me.DDataGridViewTextBoxColumn.HeaderText = "D"
+        Me.DDataGridViewTextBoxColumn.Name = "DDataGridViewTextBoxColumn"
+        Me.DDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'NotesDataGridViewTextBoxColumn
+        '
+        Me.NotesDataGridViewTextBoxColumn.DataPropertyName = "Notes"
+        Me.NotesDataGridViewTextBoxColumn.HeaderText = "Notes"
+        Me.NotesDataGridViewTextBoxColumn.Name = "NotesDataGridViewTextBoxColumn"
+        Me.NotesDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'TakeDataGridViewCheckBoxColumn
+        '
+        Me.TakeDataGridViewCheckBoxColumn.DataPropertyName = "take"
+        Me.TakeDataGridViewCheckBoxColumn.HeaderText = "take"
+        Me.TakeDataGridViewCheckBoxColumn.Name = "TakeDataGridViewCheckBoxColumn"
+        Me.TakeDataGridViewCheckBoxColumn.ReadOnly = True
+        '
+        'CorrectDataGridViewTextBoxColumn
+        '
+        Me.CorrectDataGridViewTextBoxColumn.DataPropertyName = "Correct"
+        Me.CorrectDataGridViewTextBoxColumn.HeaderText = "Correct"
+        Me.CorrectDataGridViewTextBoxColumn.Name = "CorrectDataGridViewTextBoxColumn"
+        Me.CorrectDataGridViewTextBoxColumn.ReadOnly = True
+        '
+        'QuizDataSetBindingSource
+        '
+        Me.QuizDataSetBindingSource.DataMember = "Values"
+        Me.QuizDataSetBindingSource.DataSource = Me.QuizDataSet
+        '
+        'QuizDataSet
+        '
+        Me.QuizDataSet.DataSetName = "quizDataSet"
+        Me.QuizDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+        '
+        'APTableAdapter
+        '
+        Me.APTableAdapter.ClearBeforeFill = True
+        '
+        'ComputerTableAdapter
+        '
+        Me.ComputerTableAdapter.ClearBeforeFill = True
+        '
+        'EnglishTableAdapter
+        '
+        Me.EnglishTableAdapter.ClearBeforeFill = True
+        '
+        'FilipinoTableAdapter
+        '
+        Me.FilipinoTableAdapter.ClearBeforeFill = True
+        '
+        'MapehTableAdapter
+        '
+        Me.MapehTableAdapter.ClearBeforeFill = True
+        '
+        'MathTableAdapter
+        '
+        Me.MathTableAdapter.ClearBeforeFill = True
+        '
+        'ScienceTableAdapter
+        '
+        Me.ScienceTableAdapter.ClearBeforeFill = True
+        '
+        'TLETableAdapter
+        '
+        Me.TLETableAdapter.ClearBeforeFill = True
+        '
+        'ValuesTableAdapter
+        '
+        Me.ValuesTableAdapter.ClearBeforeFill = True
+        '
+        'btnComp
+        '
+        Me.btnComp.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnComp.ForeColor = System.Drawing.Color.White
+        Me.btnComp.Location = New System.Drawing.Point(373, 97)
+        Me.btnComp.Name = "btnComp"
+        Me.btnComp.Size = New System.Drawing.Size(201, 54)
+        Me.btnComp.TabIndex = 0
+        Me.btnComp.Text = "Computer"
+        Me.btnComp.UseVisualStyleBackColor = False
+        '
+        'btnEng
+        '
+        Me.btnEng.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnEng.ForeColor = System.Drawing.Color.White
+        Me.btnEng.Location = New System.Drawing.Point(21, 172)
+        Me.btnEng.Name = "btnEng"
+        Me.btnEng.Size = New System.Drawing.Size(201, 54)
+        Me.btnEng.TabIndex = 0
+        Me.btnEng.Text = "English"
+        Me.btnEng.UseVisualStyleBackColor = False
+        '
+        'btnFil
+        '
+        Me.btnFil.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnFil.ForeColor = System.Drawing.Color.White
+        Me.btnFil.Location = New System.Drawing.Point(373, 172)
+        Me.btnFil.Name = "btnFil"
+        Me.btnFil.Size = New System.Drawing.Size(201, 54)
+        Me.btnFil.TabIndex = 0
+        Me.btnFil.Text = "Filipino"
+        Me.btnFil.UseVisualStyleBackColor = False
+        '
+        'btnMath
+        '
+        Me.btnMath.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnMath.ForeColor = System.Drawing.Color.White
+        Me.btnMath.Location = New System.Drawing.Point(21, 248)
+        Me.btnMath.Name = "btnMath"
+        Me.btnMath.Size = New System.Drawing.Size(201, 54)
+        Me.btnMath.TabIndex = 0
+        Me.btnMath.Text = "Math"
+        Me.btnMath.UseVisualStyleBackColor = False
+        '
+        'btnSci
+        '
+        Me.btnSci.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnSci.ForeColor = System.Drawing.Color.White
+        Me.btnSci.Location = New System.Drawing.Point(373, 248)
+        Me.btnSci.Name = "btnSci"
+        Me.btnSci.Size = New System.Drawing.Size(201, 54)
+        Me.btnSci.TabIndex = 0
+        Me.btnSci.Text = "Science"
+        Me.btnSci.UseVisualStyleBackColor = False
+        '
+        'btnTLE
+        '
+        Me.btnTLE.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnTLE.ForeColor = System.Drawing.Color.White
+        Me.btnTLE.Location = New System.Drawing.Point(21, 323)
+        Me.btnTLE.Name = "btnTLE"
+        Me.btnTLE.Size = New System.Drawing.Size(201, 54)
+        Me.btnTLE.TabIndex = 0
+        Me.btnTLE.Text = "TLE"
+        Me.btnTLE.UseVisualStyleBackColor = False
+        '
+        'btnMap
+        '
+        Me.btnMap.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnMap.ForeColor = System.Drawing.Color.White
+        Me.btnMap.Location = New System.Drawing.Point(373, 323)
+        Me.btnMap.Name = "btnMap"
+        Me.btnMap.Size = New System.Drawing.Size(201, 54)
+        Me.btnMap.TabIndex = 0
+        Me.btnMap.Text = "Mapeh"
+        Me.btnMap.UseVisualStyleBackColor = False
+        '
+        'btnVal
+        '
+        Me.btnVal.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnVal.ForeColor = System.Drawing.Color.White
+        Me.btnVal.Location = New System.Drawing.Point(194, 399)
+        Me.btnVal.Name = "btnVal"
+        Me.btnVal.Size = New System.Drawing.Size(201, 54)
+        Me.btnVal.TabIndex = 0
+        Me.btnVal.Text = "Values"
+        Me.btnVal.UseVisualStyleBackColor = False
+        '
+        'mainPanel
+        '
+        Me.mainPanel.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.mainPanel.Controls.Add(Me.Label1)
+        Me.mainPanel.Controls.Add(Me.btnVal)
+        Me.mainPanel.Controls.Add(Me.btnMap)
+        Me.mainPanel.Controls.Add(Me.btnTLE)
+        Me.mainPanel.Controls.Add(Me.btnSci)
+        Me.mainPanel.Controls.Add(Me.btnMath)
+        Me.mainPanel.Controls.Add(Me.btnFil)
+        Me.mainPanel.Controls.Add(Me.btnEng)
+        Me.mainPanel.Controls.Add(Me.btnComp)
+        Me.mainPanel.Controls.Add(Me.btnAP)
+        Me.mainPanel.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.mainPanel.Location = New System.Drawing.Point(13, 35)
+        Me.mainPanel.Name = "mainPanel"
+        Me.mainPanel.Size = New System.Drawing.Size(601, 487)
+        Me.mainPanel.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(224, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Label1.Font = New System.Drawing.Font("Times New Roman", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(130, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(316, 62)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Subject"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.Button1)
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(800, 534)
+        Me.Controls.Add(Me.mainPanel)
+        Me.Controls.Add(Me.DataGridView1)
         Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.QuizDataSetBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.QuizDataSet, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.mainPanel.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnAP As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents QuizDataSetBindingSource As BindingSource
+    Friend WithEvents QuizDataSet As quizDataSet
+    Friend WithEvents IDDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents QuizDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents ADataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents BDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents CDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents DDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents NotesDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents TakeDataGridViewCheckBoxColumn As DataGridViewCheckBoxColumn
+    Friend WithEvents CorrectDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+    Friend WithEvents APTableAdapter As quizDataSetTableAdapters.APTableAdapter
+    Friend WithEvents ComputerTableAdapter As quizDataSetTableAdapters.ComputerTableAdapter
+    Friend WithEvents EnglishTableAdapter As quizDataSetTableAdapters.EnglishTableAdapter
+    Friend WithEvents FilipinoTableAdapter As quizDataSetTableAdapters.FilipinoTableAdapter
+    Friend WithEvents MapehTableAdapter As quizDataSetTableAdapters.MapehTableAdapter
+    Friend WithEvents MathTableAdapter As quizDataSetTableAdapters.MathTableAdapter
+    Friend WithEvents ScienceTableAdapter As quizDataSetTableAdapters.ScienceTableAdapter
+    Friend WithEvents TLETableAdapter As quizDataSetTableAdapters.TLETableAdapter
+    Friend WithEvents ValuesTableAdapter As quizDataSetTableAdapters.ValuesTableAdapter
+    Friend WithEvents btnComp As Button
+    Friend WithEvents btnEng As Button
+    Friend WithEvents btnFil As Button
+    Friend WithEvents btnMath As Button
+    Friend WithEvents btnSci As Button
+    Friend WithEvents btnTLE As Button
+    Friend WithEvents btnMap As Button
+    Friend WithEvents btnVal As Button
+    Friend WithEvents mainPanel As Panel
+    Friend WithEvents Label1 As Label
 End Class
