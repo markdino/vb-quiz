333f436b0aba0278f230b7308e87df9164005998
MarkDino21 <markdinopelonia447@gmail.com>
2019-02-07 12:48:07 +0800

    feat: Splash screen

--- Junior-High-Quiz 2/My Project/Resources.Designer.vb
+++ Junior-High-Quiz 2/My Project/Resources.Designer.vb
@@ -157,6 +157,26 @@ Namespace My.Resources
         End Property
         
         '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property splash1() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("splash1", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
+        Friend ReadOnly Property splash2() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("splash2", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
         '''  Looks up a localized resource of type System.IO.UnmanagedMemoryStream similar to System.IO.MemoryStream.
         '''</summary>
         Friend ReadOnly Property tick() As System.IO.UnmanagedMemoryStream
---
+++ Junior-High-Quiz 2/frmSplash.vb
@@ -0,0 +1,29 @@
+﻿Public Class frmSplash
+    Dim displayCount As Integer = 0
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If Opacity < 1 Then
+            Opacity += 0.05
+        Else
+            Timer1.Interval = 1000
+            If lblProgress.Text = "Loading . . ." Then
+                lblProgress.Text = "Loading ."
+            ElseIf lblProgress.Text = "Loading ." Then
+                lblProgress.Text = "Loading . ."
+            ElseIf lblProgress.Text = "Loading . ." Then
+                lblProgress.Text = "Loading . . ."
+            End If
+            displayCount += 1
+            If displayCount = 10 Then
+                lblProgress.Text = "Completed"
+                BackgroundImage = My.Resources.splash2
+            ElseIf displayCount = 11 Then
+                frmUser.Show()
+                frmUser.Focus()
+                Close()
+
+            End If
+        End If
+
+
+    End Sub
+End Class
